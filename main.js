bad5a92af4cef35ae6fca36b04e026a6e96fa119
RamaniShankar1 <ramshankarjan1222@gmail.com>
2022-11-10 21:37:09 +0530

    Completed

---
+++ main.js
@@ -0,0 +1,14 @@
+var names = ['Ramani','Raja','Ajay','Sakthi','MariSelvan','Muthu','Sabaree','Sudesan','Vallinayagam']
+var rName=names.find(name=>name.startsWith('R'))
+console.log(rName);
+var rName=names.find(name=>name.startsWith('S'))
+console.log(rName);
+var rName=names.find(name=>name.endsWith('a'))
+console.log(rName);
+var rName=names.find(name=>name.startsWith('M'))
+console.log(rName);
+var rName=names.find(name=>name.match('y'))
+console.log(rName);
+var rName=names.find(name=>name.startsWith('V'))
+console.log(rName);
+
